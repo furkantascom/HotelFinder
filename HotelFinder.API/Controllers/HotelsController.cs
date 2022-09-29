fa8782a44fbb2fa8975a40222c047c15fdbaea60
Furkan TAŞ <frktas1214@gmail.com>
2022-09-30 01:00:45 +0300

    Add method summary for swagger

--- HotelFinder.API/Controllers/HotelsController.cs
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelFinder.API.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
     public class HotelsController : ControllerBase
@@ -15,30 +16,53 @@ namespace HotelFinder.API.Controllers
             _hotelService = hotelService;
         }
 
+        /// <summary>
+        /// Get All Hotels
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<Hotel> Get()
         {
             return _hotelService.GetAllHotels();
         }
 
+        /// <summary>
+        /// Get Hotel By ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public Hotel Get(int id)
         {
             return _hotelService.GetHotelById(id);
         }
 
+        /// <summary>
+        /// Create Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPost]
         public Hotel Post([FromBody]Hotel hotel)
         {
             return _hotelService.CreateHotel(hotel);
         }
 
+        /// <summary>
+        /// Update Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPut]
         public Hotel Put([FromBody] Hotel hotel)
         {
             return _hotelService.UpdateHotel(hotel);
         }
 
+        /// <summary>
+        /// Delete Hotel
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
